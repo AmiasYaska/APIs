f802fd05a054fe2a15b479f19d80e934175a5e7d
Amias Yaska <amiasyaska@gmail.com>
2023-09-21 14:47:05 +0300

    iss notifier

--- api_endpoints/main.py
+++ api_endpoints/main.py
@@ -29,19 +29,15 @@ sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now().hour
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
 
 
 if (MY_LAT - 5) <= iss_latitude <= (MY_LAT + 5) and (MY_LONG - 5) < iss_longitude < (MY_LONG + 5):
     if time_now >= sunset or time_now <= sunrise:
         with smtplib.SMTP("gmail.com", 587) as connection:
             connection.starttls()
-            connection.login(user="brunodanny2023@gmail.com", password="BRUNODANNY2023")
+            connection.login(user="brunodanny2023@gmail.com", password="2023")
             connection.sendmail(
-                from_addr="brunodanny2023@gmail.com",
-                to_addrs="brunodanny2023@gmail.com",
+                from_addr="br@gmail.com",
+                to_addrs="br@gmail.com",
                 msg="Subject: ISS IS CLOSE\n\nLook up and see the ISS."
             )
