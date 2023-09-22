59869a53f8bd43f3f1b6b5b411d4ed2da159e8d9
Amias Yaska <amiasyaska@gmail.com>
2023-09-22 21:10:27 +0300

    quiz app with api

--- api_quiz_app/ui.py
+++ api_quiz_app/ui.py
@@ -1,2 +1,34 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.label = Label(text="Score:0", bg=THEME_COLOR, fg="white")
+        self.label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some sample question",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="quiz_images/true.png")
+        self.true = Button(image=true_image, highlightthickness=0)
+        self.true.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="quiz_images/false.png")
+        self.false = Button(image=false_image, highlightthickness=0)
+        self.false.grid(row=2, column=1)
+
+        self.window.mainloop()
