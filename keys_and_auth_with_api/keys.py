a2543318648906f51122b04a1da8bbeece30488f
Amias Yaska <amiasyaska@gmail.com>
2023-09-24 15:33:54 +0300

    api keys

--- keys_and_auth_with_api/keys.py
+++ keys_and_auth_with_api/keys.py
@@ -1,15 +1,13 @@
 import requests
 
 parameters = {
-    "lat": 0.347596,
-    "lon": 32.582520,
+    "lat": 51.507351,
+    "lon": -0.127758,
     "appid": "4069afcb8d06a901644c5f8e85209dd3"
 }
 
-response = requests.get("https://api.openweathermap.org/data/2.5/weather", params=parameters)
-# print(response.status_code)
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response.raise_for_status()
 
-
-#
 weather_data = response.json()
 print(weather_data)
