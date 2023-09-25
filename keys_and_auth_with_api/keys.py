81229ed3a70137a05e27097592be2b2bc5dec3a5
Amias Yaska <amiasyaska@gmail.com>
2023-09-25 20:50:36 +0300

    twilio sms message

--- keys_and_auth_with_api/keys.py
+++ keys_and_auth_with_api/keys.py
@@ -1,4 +1,10 @@
 import requests
+import os
+from twilio.rest import Client
+
+account_sid = "AC7c78896325d2a1d6881a6f500a278768"
+auth_token = "b70e1fb93d966b30f053cc716ea45aec"
+
 
 parameters = {
     "lat": 0.347596,
@@ -12,9 +18,22 @@ response.raise_for_status()
 weather_data = response.json()
 weather_list = weather_data["list"][0]["weather"][0]["id"]
 
-print(weather_list)
+# print(weather_list)
+
+will_rain = False
 
 for i in weather_data["list"][:12]:
     weather_id = i["weather"][0]["id"]
     if int(weather_id) < 700:
-        print("Carry an umbrella")
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="You should carry an umbrella ☂️.",
+        from_="+12053080823",
+        to="+256700765579"
+        )
+
+    print(message.status)
