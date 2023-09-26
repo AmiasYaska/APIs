2fd8f46627d649bdb92af3462e8623e667bb9799
Amias Yaska <amiasyaska@gmail.com>
2023-09-27 01:49:25 +0300

    stock trading news

--- stock_trading_news_alert/stock_news.py
+++ stock_trading_news_alert/stock_news.py
@@ -7,7 +7,7 @@ news_url = "https://newsapi.org/v2/everything"
 
 parameters_stock = {
     "function": "TIME_SERIES_DAILY",
-    "symbol": "IBM",
+    "symbol": "TSLA",
     "apikey": "CH273F3J36IG6BXK"
 }
 
@@ -16,31 +16,34 @@ response_stock.raise_for_status()
 
 stock_data = response_stock.json()["Time Series (Daily)"]
 
-yesterday_close = float(stock_data["2023-09-25"]["4. close"])
+stock_list = [value for (key, value) in stock_data.items()]     # converts dict to list ie list comprehension
+print(stock_list)
 
-today_close = float(stock_data["2023-09-22"]["4. close"])
+yesterday_close = float(stock_list[0]["4. close"])
 
-diff_stock = ((today_close - yesterday_close) / yesterday_close) * 100
+today_close = float(stock_list[1]["4. close"])
+
+diff_stock = (abs(today_close - yesterday_close) / yesterday_close) * 100       # abs gets the magnitude
 
 print(diff_stock)
 
-if diff_stock > 5 or diff_stock < -5:
+if diff_stock > 1:
     print("Get news")
 
 
-#                   NEWS
+#                NEWS
 
-
-news_params = {
-    "q": "ibm",
-    "from": "2023-09-25",
-    "apiKey": "5bc92d3edf644d36846c615928f45c1b",
-}
-
-response_news = requests.get(news_url, params=news_params)
-response_news.raise_for_status()
-
-news_data = response_news.json()["articles"]
-
-top_3_news = news_data[:2]
-print(top_3_news)
+#
+# news_params = {
+#     "q": "tesla",
+#     "from": "2023-09-25",
+#     "apiKey": "5bc92d3edf644d36846c615928f45c1b",
+# }
+#
+# response_news = requests.get(news_url, params=news_params)
+# response_news.raise_for_status()
+#
+# news_data = response_news.json()["articles"]
+#
+# top_3_news = news_data[:3]
+# print(top_3_news)
