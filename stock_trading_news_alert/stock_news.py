395f4afe72e20cf5f5262d448814a9ef273134c3
Amias Yaska <amiasyaska@gmail.com>
2023-09-26 20:48:11 +0300

    stock trading news

--- stock_trading_news_alert/stock_news.py
+++ stock_trading_news_alert/stock_news.py
@@ -1,7 +1,9 @@
 import requests
+from newsapi import NewsApiClient
 import datetime as dt
 
 stock_url = "https://www.alphavantage.co/query"
+news_url = "https://newsapi.org/v2/everything"
 
 parameters_stock = {
     "function": "TIME_SERIES_DAILY",
@@ -9,10 +11,10 @@ parameters_stock = {
     "apikey": "CH273F3J36IG6BXK"
 }
 
-response = requests.get(stock_url, params=parameters_stock)
-response.raise_for_status()
+response_stock = requests.get(stock_url, params=parameters_stock)
+response_stock.raise_for_status()
 
-stock_data = response.json()["Time Series (Daily)"]
+stock_data = response_stock.json()["Time Series (Daily)"]
 
 yesterday_close = float(stock_data["2023-09-25"]["4. close"])
 
@@ -24,3 +26,21 @@ print(diff_stock)
 
 if diff_stock > 5 or diff_stock < -5:
     print("Get news")
+
+
+#                   NEWS
+
+
+news_params = {
+    "q": "ibm",
+    "from": "2023-09-25",
+    "apiKey": "5bc92d3edf644d36846c615928f45c1b",
+}
+
+response_news = requests.get(news_url, params=news_params)
+response_news.raise_for_status()
+
+news_data = response_news.json()["articles"]
+
+top_3_news = news_data[:2]
+print(top_3_news)
