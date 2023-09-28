2d085e17c6ff0eee4748c846be7695b6b7fbfdbd
Amias Yaska <amiasyaska@gmail.com>
2023-09-28 13:17:55 +0300

    habit tracking app using pixela

--- habit_tracking_api/post_request.py
+++ habit_tracking_api/post_request.py
@@ -1,9 +1,14 @@
 import requests
+from datetime import datetime
+
+#                           create a user
 
 pixela_url = "https://pixe.la/v1/users"
 
 TOKEN = "bsidbdu764hdih"
 USERNAME = "donnyvan"
+
+GRAPH_ID = "graph1"
 
 parameter = {
     "token": TOKEN,
@@ -15,12 +20,12 @@ parameter = {
 # response = requests.post(pixela_url, json=parameter)
 # print(response.text)
 
-#               post headers
+#               post headers eg graph
 
 graph_url = f"{pixela_url}/{USERNAME}/graphs"
 
 graph_params = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Graph",
     "unit": "hours",
     "type": "float",
@@ -31,5 +36,28 @@ headers_param = {
     "X-USER-TOKEN": TOKEN
 }
 
-response_graph = requests.post(url=graph_url, json=graph_params, headers=headers_param)
-print(response_graph.text)
+# response_graph = requests.post(url=graph_url, json=graph_params, headers=headers_param)
+# print(response_graph.text)
+
+
+#                   post a pixel
+
+pixel_url = f"{pixela_url}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_params = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "1.5",
+}
+
+# pixel_response = requests.post(url=pixel_url, json=pixel_params, headers=headers_param)
+# print(pixel_response.text)
+
+
+
+
+#                       FORMATTING DATE
+# today = dt.datetime.now()
+# formatted_today = today.strftime("%Y-%m-%d")
+# print(formatted_today)
