d60bc0e138f50fba349d6dbb03a41b2619853466
Amias Yaska <amiasyaska@gmail.com>
2023-09-28 13:53:59 +0300

    habit tracking app using pixela

--- habit_tracking_api/post_request.py
+++ habit_tracking_api/post_request.py
@@ -1,12 +1,12 @@
 import requests
 from datetime import datetime
 
-#                           create a user
+#                           CREATE A USER
 
 pixela_url = "https://pixe.la/v1/users"
 
-TOKEN = "bsidbdu764hdih"
-USERNAME = "donnyvan"
+TOKEN = "bsidbd"
+USERNAME = "don"
 
 GRAPH_ID = "graph1"
 
@@ -17,10 +17,10 @@ parameter = {
     "notMinor": "yes"
 }
 
-# response = requests.post(pixela_url, json=parameter)
-# print(response.text)
+response = requests.post(pixela_url, json=parameter)
+print(response.text)
 
-#               post headers eg graph
+#               POST HEADERS eg graph
 
 graph_url = f"{pixela_url}/{USERNAME}/graphs"
 
@@ -36,11 +36,11 @@ headers_param = {
     "X-USER-TOKEN": TOKEN
 }
 
-# response_graph = requests.post(url=graph_url, json=graph_params, headers=headers_param)
-# print(response_graph.text)
+response_graph = requests.post(url=graph_url, json=graph_params, headers=headers_param)
+print(response_graph.text)
 
 
-#                   post a pixel
+#                   POST PIXEL
 
 pixel_url = f"{pixela_url}/{USERNAME}/graphs/{GRAPH_ID}"
 
@@ -51,13 +51,29 @@ pixel_params = {
     "quantity": "1.5",
 }
 
-# pixel_response = requests.post(url=pixel_url, json=pixel_params, headers=headers_param)
-# print(pixel_response.text)
+pixel_response = requests.post(url=pixel_url, json=pixel_params, headers=headers_param)
+print(pixel_response.text)
 
+#                       UPDATE PIXEL
+
+update_url = f"{pixel_url}/{today.strftime('%Y%m%d')}"
+
+update_params = {
+    "quantity": "1.5"
+}
+
+update_response = requests.put(url=update_url, json=update_params, headers=headers_param)
+print(update_response.text)
+
+#                       DELETE A PIXEL
+
+delete_response = requests.delete(url=update_url, headers=headers_param)
+print(delete_response.text)
 
 
 
 #                       FORMATTING DATE
-# today = dt.datetime.now()
-# formatted_today = today.strftime("%Y-%m-%d")
-# print(formatted_today)
+
+today = datetime.now()
+formatted_today = today.strftime("%Y-%m-%d")        # or strftime("%y%m%d") or any other
+print(formatted_today)
